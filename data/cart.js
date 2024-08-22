30a238c74b71f7d75133796cf34000c0a7e6c439
Nafeed <mohammadnafeed013@gmail.com>
2024-08-22 18:42:42 +0530

    move addTocart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,24 @@
 export const cart = [];
+
+// ...adding products to cart...
+export function addToCart(productId) {
+    let matchingItem;
+  
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+  
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      let quantity = Number(quantitySelector.value);
+   
+      if (matchingItem) {
+        matchingItem.quantity += quantity;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: quantity
+        });
+      }
+  }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,9 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
 
+//...Generating HTML for each products...
 products.forEach((product) => {
     productsHTML += `
     <div class="product-container">
@@ -57,46 +58,34 @@ products.forEach((product) => {
     `;
 });
 
+//...Displaying products after generating HTML
 document.querySelector('.js-products-grid').innerHTML = productsHTML; 
 
+//...Updating number of products in the cart...
+function updateCartQuantity(productId) {
+  let cartQuantity = 0;
+
+
+  cart.forEach((item) => {
+    cartQuantity += item.quantity
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+  const addedMessage = document.querySelector(`.js-visible-cart-${productId}`);
+  addedMessage.classList.add('added-to-cart-visible');
+
+  setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
+  },2000);
+}
+
+//...Code for add-to-cart button...
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${button.dataset.productId}`);
-    let quantity = Number(quantitySelector.value);
- 
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: quantity
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-    const addedMessage = document.querySelector(`.js-visible-cart-${productId}`);
-    addedMessage.classList.add('added-to-cart-visible');
-
-    setTimeout(() => {
-      addedMessage.classList.remove('added-to-cart-visible');
-    },2000);
+    addToCart(productId);
+    updateCartQuantity(productId);
 
   });
 });
